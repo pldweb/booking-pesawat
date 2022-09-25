a8710fb922d635af6f8f0a052a3a8a631c9557cb
paldi <paldi@gmail.com>
2022-09-25 13:24:10 +0700

    done slicing page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:booking_pesawat/ui/pages/checkout_page.dart';
 import 'package:booking_pesawat/ui/pages/choose_seat_page.dart';
+import 'package:booking_pesawat/ui/pages/success_checkout_page.dart';
 import 'package:flutter/material.dart';
 import 'package:booking_pesawat/ui/pages/bonus_page.dart';
 import 'package:booking_pesawat/ui/pages/home_page.dart';
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
         '/home': (context) => HomePage(),
         '/choose': (context) => ChooseSeatPage(),
         '/checkout': (context) => CheckoutPage(),
+        '/success': (context) => SuccessCheckoutPage(),
       },
     );
   }
